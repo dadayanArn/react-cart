80451233eea4126dae7f1649ad45ae1ad040fc4a
ArnodDadayan <dadayanaa@yahoo.com>
2019-11-17 14:51:44 +0400

    Updates

---
+++ precache-manifest.f4561b82a24b4abfb60968ceaa8f7eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67daf6a4c9a6e5229eb342621a2623d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f12002e40929e1e7934",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "fe32eaf8aa349561bd25",
+    "url": "/static/css/main.0aa7c5f9.chunk.css"
+  },
+  {
+    "revision": "9f12002e40929e1e7934",
+    "url": "/static/js/2.be5daf9a.chunk.js"
+  },
+  {
+    "revision": "fe32eaf8aa349561bd25",
+    "url": "/static/js/main.792bde99.chunk.js"
+  },
+  {
+    "revision": "e98eef5ac3daff9c5945",
+    "url": "/static/js/runtime-main.b9fe458b.js"
+  }
+]);
